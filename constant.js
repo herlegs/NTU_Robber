e1fb5bdc20acb336d024c8ccb7ba182f9944828c
Xiao <xiao_y@worksap.co.jp>
2016-04-13 19:59:16 +0800

    working version

--- constant.js
+++ constant.js
@@ -14,21 +14,73 @@ var NTURobber = NTURobber ? NTURobber : {util: {}, constant: {}};
 		PRE_LOGIN: 0,
 		USERNAME: 1,
 		PASSWORD: 2,
-		CATEGORY: 3
+		CATEGORY: 3,
+		CHECK_SLOT: 4
 	};
 
 	constant.CATEGORY = {
-		badminton: {id: 0, key: "badminton", name: "Badminton"},
-		squash: {id: 1, key: "squash", name: "Squash"},
-		table_tennis_at_srctr: {id: 2, key: "table_tennis_at_srctr", name: "Table Tennis Table(S & R Ctr)"},
-		table_tennis_at_squash: {id: 3, key: "table_tennis_at_squash", name: "Table Tennis Table(At Squash Court 4 during period 8 Apr to 13 May 16)"},
-		tennis_at_hall7: {id: 4, key: "tennis_at_hall7", name: "Tennis Court(Hall 7)"},
-		tennis_at_srctr: {id: 5, key: "tennis_at_srctr", name: "Tennis Court(S & R Ctr)"}
+		badminton: {id: 0, key: "badminton", name: "Badminton", range: [2, 19], tables: 4},
+		squash: {id: 1, key: "squash", name: "Squash", range: [2, 19], tables: 4},
+		table_tennis_at_srctr: {id: 2, key: "table_tennis_at_srctr", name: "Table Tennis Table(S & R Ctr)", range: [2, 19], tables: 4},
+		table_tennis_at_squash: {id: 3, key: "table_tennis_at_squash", name: "Table Tennis Table(At Squash Court 4 during period 8 Apr to 13 May 16)", range: [2, 19], tables: 2},
+		tennis_at_hall7: {id: 4, key: "tennis_at_hall7", name: "Tennis Court(Hall 7)", range: [2, 18], tables: 2},
+		tennis_at_srctr: {id: 5, key: "tennis_at_srctr", name: "Tennis Court(S & R Ctr)", range: [0, 19], tables: 6}
 	}
 
 	constant.TIMESLOT = {
-		
+		"07:30" : 0,
+		"08:15" : 1,
+		"09:00" : 2,
+		"09:45" : 3,
+		"10:30" : 4,
+		"11:15" : 5,
+		"12:00" : 6,
+		"12:45" : 7,
+		"13:30" : 8,
+		"14:15" : 9,
+		"15:00" : 10,
+		"15:45" : 11,
+		"16:30" : 12,
+		"17:15" : 13,
+		"18:00" : 14,
+		"18:45" : 15,
+		"19:30" : 16,
+		"20:15" : 17,
+		"21:00" : 18,
+		"21:45" : 19
 	}
+
+	constant.TIMESLOT_array = ["07:30","08:15","09:00","09:45","10:30","11:15","12:00","12:45","13:30","14:15","15:00","15:45","16:30","17:15","18:00","18:45","19:30","20:15","21:00","21:45"];
+
+	constant.MONTH_NAME_array = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+	constant.getTimeListFromRange = function(range){
+		return constant.TIMESLOT_array.slice(range[0], range[1] + 1);
+	}
+
+	//get table row range from user's chosen time range
+	constant.getTRRange = function(categoryKey, range){
+		var trrange = [0, 0];
+		var startIndex = 0;
+		var endIndex = 0;
+		var category = constant.CATEGORY[categoryKey];
+		if(category.range[0] > range[0] || category.range[1] < range[1]){
+			return trrange;
+		}
+		startIndex = range[0] - category.range[0];
+		endIndex = range[1] - range[0];
+		trrange = [startIndex*category.tables, endIndex*category.tables];
+		return trrange;
+	}
+
+	constant.getExpectedStringFromDate = function(date){
+		var day = date.getDate();
+		var month = constant.MONTH_NAME_array[date.getMonth()];
+
+		return "" + day + month;
+	}
+
+
 
 })();
 
--- fillin.js
+++ fillin.js
@@ -1,7 +1,12 @@
 (function($) {
 	var constant = NTURobber.constant;
 
-	var choosenCategory = constant.CATEGORY.badminton;
+	var choosenCategory = constant.CATEGORY.squash;
+
+	var choosenDate = new Date();
+	choosenDate.setDate(choosenDate.getDate() + 1);
+
+	var choosenRange = [2, 4];
 
 	var interval_default = 1000;
 
@@ -16,7 +21,9 @@
 
 	var login_ok_btn = 'input[name="bOption"]';
 
-	var category_list = "input[name='p_info']";
+	var category_list = "form[action='srce_smain_s.srce_temp']";
+
+	var time_slot_form = "form[action='srce_sub1.srceb$sel32']";
 
 	var watcher;
 
@@ -52,8 +59,11 @@
 		if(isElementExist(category_list)){
 			return {
 				stage: constant.STAGE.CATEGORY,
-				category_list: $(category_list)
+				category_list: $("input[name='p_info']")
 			}
+		}
+		if(isElementExist(time_slot_form)){
+			return {stage: constant.STAGE.CHECK_SLOT}
 		}
 	}
 
@@ -85,11 +95,69 @@
 		var categoryBtn = stageInfo.category_list.eq(choosenCategory.id);
 		categoryBtn.get(0).click();
 	}
+	else if(stageInfo.stage == constant.STAGE.CHECK_SLOT){
+        trySelectSlot();
+	}
     
     startWatcher(nextTaskDelay);
   }
 
+    function trySelectSlot(){
+        var targetCells = getTargetCells();
+        var slots = getValidSlot(targetCells);
+        if(slots.length){
+            slots[0].get(0).click();
+        }
+        else{
+            alert("no slots");
+        }
+    }
 
+    function getTargetCells(){
+        var targetCells = [];
+        var trrange = constant.getTRRange(choosenCategory.key, choosenRange);
+        var table = $(time_slot_form).find("table:eq(1)");
+        var rows = table.find("tr");
+        var headerRow = rows.eq(0);
+        var columnIndex = getColumnIndexFromChosenDate(headerRow);
+
+        if(columnIndex == 0){
+            return targetCells;
+        }
+
+        var targetRows = rows.slice(trrange[0] + 1, trrange[1] + 1);
+        for(var i = 0; i < targetRows.length; i++){
+            var cells = targetRows.eq(i).find("td");
+            var cell = cells.length == 10 ? cells.eq(columnIndex) : cells.eq(columnIndex - 1);
+            targetCells.push(cell);
+        }
+        return targetCells;
+    }
+
+    function getValidSlot(cells){
+        var slots = [];
+        for(var i = 0; i < cells.length; i++){
+            var cell = cells[i];
+            var slot = cell.find("input");
+            if(slot.length){
+                slots.push(slot);
+            }
+        }
+        return slots;
+    }
+
+    function getColumnIndexFromChosenDate(headerRow){
+        var columnIndex = 0;
+        var headerCells = headerRow.find("td");
+        var chosenDateString = constant.getExpectedStringFromDate(choosenDate);
+        for(var i = 0; i < headerCells.length; i++){
+            if(headerCells.eq(i).text().startsWith(chosenDateString)){
+                columnIndex = i;
+                break;
+            }
+        }
+        return columnIndex;
+    }
 
   function startListener(){
     chrome.runtime.onMessage.addListener(
